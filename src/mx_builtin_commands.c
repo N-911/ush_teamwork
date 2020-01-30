831a9cebe216a8a8c2661546a970be64bfbb9c65
Maksym Libovych <mlibovych@e2r13p6.unit.ua>
2020-01-30 16:54:52 +0200

    exit fix 02

--- src/mx_builtin_commands.c
+++ src/mx_builtin_commands.c
@@ -26,13 +26,26 @@ int mx_jobs(t_shell *m_s, t_process *p) {
 
 int mx_exit(t_shell *m_s, t_process *p) {
     int exit_code = m_s->exit_code;
+    int flag = 0;
+    int start = 0;
 
     if(p->argv[1] != NULL && p->argv[2] != NULL) {
         mx_printerr("ush: exit: too many arguments\n");
         return 1;
     }
     if (p->argv[1] != NULL) {
-        if (atoi(p->argv[1])) {
+        if (p->argv[1][0] == '+' || p->argv[1][0] == '-') {
+            if (p->argv[1][1] == '\0')
+                flag++;
+            start++;
+        }
+        for (int i = start; i < mx_strlen(p->argv[1]); i++) {
+            if(!mx_isdigit(p->argv[1][i])) {
+                flag++;
+                break;
+            }
+        }
+        if (!flag) {
             exit_code = atoi(p->argv[1]);
         }
         else {
