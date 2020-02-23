b3358e2e3e9f26b31ba3647a418a06a23c1ccc3c
NHokage <maksimlibovich@ukr.net>
2020-02-23 19:22:25 +0200

    crush fixed

---
+++ mx_ush_read_line.c
@@ -0,0 +1,14 @@
+#include "ush.h"
+/*
+ * Read line from stdin except last char ('\n').
+ */
+char *mx_ush_read_line(void) {
+    char *line = NULL;
+    char *res = NULL;
+    size_t bufsize = 0;  // have getline allocate a buffer for us
+
+    getline(&line, &bufsize, stdin);
+    res = mx_strndup(line, mx_strlen(line) - 1);
+    mx_strdel(&line);
+    return res;
+}
